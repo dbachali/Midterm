a65fdb62390b838cf5d10903dff4139c16200fcb
dbachali <dbachali@stevens.edu>
2021-05-09 23:36:55 -0400

    add error handling

--- main.py
+++ main.py
@@ -22,6 +22,15 @@ def help():
         html = markdown.markdown(text)
     return html
 
+@app.errorhandler(404)
+def not_found_error(error):
+    return render_template('404.html'), 404
+
+@app.errorhandler(500)
+def internal_error(error):
+    db.session.rollback()
+    return render_template('500.html'), 500
+
 @app.route('/test/<text>', methods=['GET', 'POST'])
 # test definition to verify that the service is running and accepting a given string
 def test(text):
