a90eb87a028aff670beed6b2dafd57372cfbba18
dbachali <dbachali@stevens.edu>
2021-05-09 21:50:52 -0400

    add error handling

--- main.py
+++ main.py
@@ -8,27 +8,35 @@ from textblob import TextBlob
 
 app = Flask(__name__)
 
+# create routes for nlp services on a given string
 @app.route('/test/<text>', methods=['GET', 'POST'])
+# test definition to verify that the service is running and accepting a given string
 def test(text):
     return render_template("sample.html", name = text, Title = "test")
 
 @app.route('/sentiment/<text>', methods=['GET', 'POST'])
+# sentiment analysis on a given string
 def sentiment(text):
     return render_template("sample.html", name = TextBlob(text).sentiment, Title = "sentiment", text = text)
 
 @app.route('/polarity/<text>', methods=['GET', 'POST'])
+# polarity of a given string
 def polarity(text):
     return render_template("sample.html", name = TextBlob(text).polarity, Title = "polarity", text = text)
 
 @app.route('/subjectivity/<text>', methods=['GET', 'POST'])
+# subjectivity of a given string
 def subjectivity(text):
     return render_template("sample.html", name = TextBlob(text).subjectivity, Title = "subjectivity", text = text)
 
 @app.route('/ngrams/<text>/<int:num>', methods=['GET', 'POST'])
+# Split a given string into ngrams of a given size
 def ngrams(text, num):
-    if num > len(text.split()):
-        return "The ngram number must be >= the number of words in text."
+    # Check that num isn't greater than the number or words in text, which would result in a blank screen
+    if num > len(text.split()) or num < 1 or not isinstance(num, int):
+        return "The ngram number must be non-zero, positive and >= the number of words in text."
 
+    # Create a list to fill with ngrams that can be easily read and displayed in html
     list = []
     list_ngrams = TextBlob(text).ngrams(num)
     for l in list_ngrams:
