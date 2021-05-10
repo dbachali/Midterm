32c63f7657d6c87534d600274f5ad4a12101a86e
dbachali <dbachali@stevens.edu>
2021-05-09 21:24:05 -0400

    test

--- main.py
+++ main.py
@@ -10,19 +10,19 @@ app = Flask(__name__)
 
 @app.route('/test/<text>', methods=['GET', 'POST'])
 def test(text):
-    return render_template("sample.html", name = text)
+    return render_template("sample.html", name = text, Title = "test")
 
 @app.route('/sentiment/<text>', methods=['GET', 'POST'])
 def sentiment(text):
-    return render_template("sample.html", name = TextBlob(text).sentiment)
+    return render_template("sample.html", name = TextBlob(text).sentiment, Title = "sentiment", text = text)
 
 @app.route('/polarity/<text>', methods=['GET', 'POST'])
 def polarity(text):
-    return render_template("sample.html", name = TextBlob(text).polarity)
+    return render_template("sample.html", name = TextBlob(text).polarity, Title = "polarity", text = text)
 
 @app.route('/subjectivity/<text>', methods=['GET', 'POST'])
 def subjectivity(text):
-    return render_template("sample.html", name = TextBlob(text).subjectivity)
+    return render_template("sample.html", name = TextBlob(text).subjectivity, Title = "subjectivity", text = text)
 
 @app.route('/ngrams/<text>/<int:num>', methods=['GET', 'POST'])
 def ngrams(text, num):
@@ -35,7 +35,7 @@ def ngrams(text, num):
         l = str(l)
         list.append(l)
 
-    return render_template("list.html", list = list )
+    return render_template("list.html", list = list, Title = "ngrams", text = text)
 
 
 if __name__ == '__main__':
