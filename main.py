76bcb799d29c07ee99907424c676d63b0193df88
dbachali <dbachali@stevens.edu>
2021-05-09 23:42:43 -0400

    add error handling

--- main.py
+++ main.py
@@ -26,9 +26,9 @@ def help():
 def not_found_error(error):
     return render_template('404.html'), 404
 
-@app.errorhandler(500)
-def internal_error(error):
-    return render_template('500.html'), 500
+#@app.errorhandler(500)
+#def internal_error(error):
+#    return render_template('500.html'), 500
 
 @app.route('/test/<text>', methods=['GET', 'POST'])
 # test definition to verify that the service is running and accepting a given string
